9649f2f138d5b70c9f2520cccd5b73c63cf51d8f
Benjamin A. Beasley <code@musicinmybrain.net>
2025-10-15 23:22:04 +0100

    Make snapshot tests order-insensitive
    Sort walked paths since the order in which they are generated may vary.
    
    We could have compared sets of paths, but the sorting approach will
    still detect duplicate paths, should they ever occur.

--- tests/test_walk.py
+++ tests/test_walk.py
@@ -38,7 +38,7 @@ def folder(tmp_path: str, folder_structure: list[str]) -> Path:
 
 
 def test_basic_walk(folder: Path, folder_structure: list[str]):
-    paths = [str(path.relative_to(folder)) for path in rignore.walk(folder)]
+    paths = sorted(str(path.relative_to(folder)) for path in rignore.walk(folder))
 
     assert paths == snapshot(
         [
@@ -57,18 +57,18 @@ def test_filter_entry(folder: Path):
     def should_exclude(entry: Path) -> bool:
         return entry.name == "some_folder"
 
-    paths = [
+    paths = sorted(
         str(path.relative_to(folder))
         for path in rignore.walk(folder, should_exclude_entry=should_exclude)
-    ]
+    )
 
     assert paths == snapshot([".", "an_image.jpg", "some_file.txt"])
 
 
 def test_overrides(folder: Path):
-    paths = [
+    paths = sorted(
         str(path.relative_to(folder))
         for path in rignore.walk(folder, overrides=[".hidden"])
-    ]
+    )
 
     assert paths == snapshot([".", ".hidden", "some_folder", "some_folder/some_folder"])
